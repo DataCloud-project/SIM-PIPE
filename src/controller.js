f7eea2acff9ab11027dbef7b8a03784d82226dee
Aleena Thomas <84079188+AleenaThomas-gh@users.noreply.github.com>
2021-12-06 10:35:36 +0100

    Update controller.js

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,13 @@
 var Docker = require('dockerode');
 var fs     = require('fs');
+let config = require('config')
+const sender = require('./send_input'); 
+const { start } = require('repl');
+const {execSync} = require('child_process');
+
+localFile = config.get('sample_input_file')
+remoteFile = config.get('remote_sftp_dir') + '/in/input.txt'
+
 
 const remote = true;
 var docker = null;
@@ -22,14 +30,66 @@ if(remote) {
   docker = new Docker({ socketPath: socket });
 }
 
-//Volume specified in docker createcontainer function using Binds parameter
+
+// execSync(sender.send_input(localFile, remoteFile));
+// .then(() => 
+var created_container; 
 docker.createContainer({
-  Image: 'i1',
+  Image: config.get('image'),
+  Tty: true,  
+  //Volume specified in docker createcontainer function using Binds parameter
+  Binds: ['/var/lib/docker/volumes/volume_vm/_data/in:/app/in', 
+          '/var/lib/docker/volumes/volume_vm/_data/out:/app/out',
+          '/var/lib/docker/volumes/volume_vm/_data/work:/app/work'],
+})
+.then((container) => {created_container = container; return container.start({})})
+.then(() => {
+  let ID = created_container.id;  
+  console.log('Container ID: ', ID);
+  created_container.wait();
+  return ID;
+})
+.then((ID) => {
+  created_container.logs({follow: false,stdout: true,stderr: true,stdin: true}, (err, stream) => {
+    if(err) {
+      return logger.error(err.message);
+    }
+    let filename = './logs_'+ ID +'.txt'	//filename with container id
+    fs.writeFile(filename, stream.toString('utf8'), (err, result) => {
+      if(err) console.log('error', err);
+    });
+    console.log('Container logs saved to file', filename);
+  
+  });
+});
+
+
+// async function controller() {
+//   await start_container();
+
+// }
+
+// function start_container() {
+//   return new Promise(resolve => {
+//     container.start();
+//     resolve();
+//   });
+// }
+
+// function wait_for_container(container) {
+//   let ID = container.id  
+//   console.log('Container ID: ', ID);
+//   container.wait(
+// }
+/*
+docker.createContainer({
+  Image: config.get('image'),
   Tty: true,  
   Binds: ['/var/lib/docker/volumes/volume_vm/_data/in:/app/in', 
           '/var/lib/docker/volumes/volume_vm/_data/out:/app/out',
           '/var/lib/docker/volumes/volume_vm/_data/work:/app/work'],
 }, (err, container) => {
+    
       container.start({}, (err, data) => {  
         let ID = container.id  
         console.log('Container ID: ', ID);
@@ -50,8 +110,6 @@ docker.createContainer({
         });    
       });
 });
-
-/*
  volumes: 'volume_vm:/app'
  volumes: ['volume_vm:/app']
  volumes: {'volume_vm:/app'} - compile error
