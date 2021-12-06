574905aa79f0411a3a3b5bbf694ebea75a2ea85f
aleenathomas <aleenathomas203@gmail.com>
2021-12-06 10:48:38 +0100

    Updated sftp server setup

--- src/temp.js
+++ src/temp.js
@@ -1,25 +1,25 @@
-var Client = require('ssh2').Client;
-
-var connSettings = {
-    host: 'localhost',
-    port: 2222, 
-    username: 'user1',
-    password: 'user1'
-    // key file
-};
-
-var conn = new Client();
-conn.on('ready', function() {
-    conn.sftp(function(err, sftp) {
-        //  if (err) throw err;
-         // into sftp
-         sftp.readdir('/home/user1/_data', function(err, list) {
-            // if (err) throw err;
-            // List the directory in the console
-            console.log(list);
-            console.dir(list);
-            // Do not forget to close the connection, otherwise you'll get troubles
-            conn.end();
-     });
+function one() {
+    return new Promise(resolve => {
+      return 10;
+      resolve();
     });
-}).connect(connSettings);
+  }
+  
+  function two(num) {
+    return new Promise(resolve => {
+      console.log(num);
+      return 5;
+      resolve();
+    });
+  }
+  
+  function three(num){
+     console.log(num);
+  }
+
+  async function run() {
+    await one();
+    await two(num);
+    three(num);
+  }
+  run();
