c34d370ce1a0ef0bd806ef695116344498e75ead
AleenaThomas_gh <aleena.thomas@sintef.no>
2021-12-10 19:33:47 +0100

    Parser added
    - parsed the generated stats files to get statistics.json file of the metrics with time stamp.

--- src/controller.js
+++ src/controller.js
@@ -3,6 +3,8 @@ var fs     = require('fs');
 let config = require('config')
 let _ = require('lodash/core');
 let async = require("async");
+const path = require('path'); 
+
 
 // localFile = config.get('input_file')
 // const remoteInputFile = 'in/input.txt';
@@ -32,6 +34,7 @@ const sim_id = config.get("sim_id");
 const run_id = config.get("run_id");
 const step_number = config.get("step_number");
 const target_dir = './' + sim_id + '/' + run_id + '/' + step_number;
+const pollingInterval = 500;
 
 // execSync(sender.send_input(localFile, remoteFile));
 // .then(() => 
@@ -47,7 +50,7 @@ docker.createContainer({
 .then((container) => {created_container = container; return container.start({})})
 .then(() => {
   console.log('Container created with ID: ', created_container.id);
-  statsInterval = setInterval(function(){ getStats(created_container) }, 1000);    
+  statsInterval = setInterval(function(){ getStats(created_container) }, pollingInterval);    
 });
 
 async function getStats(created_container) {
@@ -56,15 +59,13 @@ async function getStats(created_container) {
     let ids = [];
     containers.forEach(function (containerInfo) {
       ids.push(docker.getContainer(containerInfo.Id).id);
-      // console.log('id', docker.getContainer(containerInfo.Id).id);
     });
-    // console.log(ids);
-    // console.log(""+created_container.id);
+
     if(!ids.includes("" + created_container.id)) {
-      // console.log('Stopping stats');
       stopStats();
-      // console.log('getting logs');
-      // collect logs of th stoppped container
+      parseStats();
+
+      // collect logs of the stoppped container
       created_container.logs({follow: false,stdout: true,stderr: true,stdin: true}, (err, stream) => {
         if(err) {
           return logger.error(err.message);
@@ -78,19 +79,13 @@ async function getStats(created_container) {
     }
     // collect statstics as long as the container is running
     else {  
-      // console.log('Collecting statistics');
       created_container.stats({ stream: false }, function (err, stream) {
         if (err) { console.log('error'); }
-        var filename = target_dir + '/stats.' + counter + '.txt';
+        var filename = target_dir + '/stats.' + counter + '.json';
         counter = counter + 1;
-        var subset = _.pick(stream, ['cpu_stats', 'memory_stats', 'name', 'id']); //store only required details
-        // var subset = _.pick(_.flatten(stream), ['cpu_stats:{cpu_usage}', 'memory_stats', 'name', 'id']); //store only required details
-        // fs.writeFile(filename, JSON.stringify(subset, null, ' '), (err, result) => {
-        fs.writeFile(filename, JSON.stringify(subset, null, ' '), (err, result) => {
+        fs.writeFile(filename, JSON.stringify(stream, null, ' '), (err, result) => {
           if(err) console.log('error', err);
         });
-        // console.log(stream);
-        console.log('Saved to file', filename);
      });
     }
   }); 
@@ -100,3 +95,36 @@ function stopStats() {
   clearInterval(statsInterval);
 }
 
+async function parseStats() {
+  const dirName = target_dir;
+  fileList = fs.readdirSync(dirName);
+  let stats = [];
+  for (let i = 0; i < fileList.length; i++) {
+    const filename = fileList[i];
+    if (!filename.startsWith("stats")) continue;
+
+    const fullFilename = path.join(dirName, filename);
+    data = fs.readFileSync(fullFilename, { encoding: "utf-8" });
+      
+        try {
+          const fileContent = JSON.parse(data);
+          if(fileContent['read'].startsWith("0001-01-01T00:00:00Z"))
+            break;
+          
+          let timestamp = fileContent['read'];
+          let cpu = fileContent['cpu_stats']['cpu_usage']['total_usage'];
+          let memory = fileContent['memory_stats']['usage'];
+          let memory_max = fileContent['memory_stats']['max_usage'];
+          let net = fileContent['networks']['eth0']['rx_bytes'];
+          stats.push({timestamp, cpu, memory, memory_max, net});
+          
+        } catch (err) {
+          console.error(`error while parsing file: ${fullFilename}`);
+        }
+       
+  }
+  console.log(stats);
+  let json = JSON.stringify(stats, null, ' ');
+  fs.appendFileSync(target_dir + '/statistics.json', json);
+
+}
