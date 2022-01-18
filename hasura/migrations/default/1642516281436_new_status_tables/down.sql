2faa0168a1d805312282845c3aeafde0eb0d3103
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2022-01-18 15:34:34 +0100

    🗃Add tables run_status and step_status instead of PostGreSQL enums

---
+++ hasura/migrations/default/1642516281436_new_status_tables/down.sql
@@ -0,0 +1,54 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- ALTER TABLE simpipe.runs DROP CONSTRAINT "ended started constraint";
+--
+-- ALTER TABLE simpipe.runs ALTER COLUMN status TYPE text;
+-- ALTER TABLE simpipe.steps ALTER COLUMN status TYPE text;
+--
+-- ALTER TABLE simpipe.runs ALTER COLUMN status SET DEFAULT 'waiting';
+-- ALTER TABLE simpipe.steps ALTER COLUMN status SET DEFAULT 'waiting';
+--
+-- DROP TYPE simpipe.run_status;
+-- DROP TYPE simpipe.step_status;
+--
+-- ALTER TABLE simpipe.runs ADD CONSTRAINT "ended started constraint" CHECK (
+--   (status = 'waiting' AND (started IS NULL) AND (ended IS NULL)) OR
+--   (status = 'active' AND (started IS NOT NULL) AND (ended IS NULL)) OR
+--   (status = 'completed' AND (started IS NOT NULL) AND (ended IS NOT NULL)) OR
+--   ((status = 'failed' OR status = 'cancelled') AND
+--     ((ended IS NULL) OR (started IS NOT NULL))));
+--
+-- ALTER TABLE simpipe.steps ADD CONSTRAINT "ended started constraint" CHECK (
+--   (status = 'waiting' AND (started IS NULL) AND (ended IS NULL)) OR
+--   (status = 'active' AND (started IS NOT NULL) AND (ended IS NULL)) OR
+--   (status = 'completed' AND (started IS NOT NULL) AND (ended IS NOT NULL)) OR
+--   ((status = 'failed' OR status = 'cancelled') AND
+--     ((ended IS NULL) OR (started IS NOT NULL))));
+--
+-- CREATE TABLE simpipe.run_status (
+--   value text PRIMARY KEY
+-- );
+--
+-- INSERT INTO simpipe.run_status (value) VALUES
+--   ('waiting'),
+--   ('active'),
+--   ('completed'),
+--   ('failed'),
+--   ('cancelled');
+--
+-- CREATE TABLE simpipe.step_status (
+--   value text PRIMARY KEY
+-- );
+--
+-- INSERT INTO simpipe.step_status (value) VALUES
+--   ('waiting'),
+--   ('active'),
+--   ('completed'),
+--   ('failed'),
+--   ('cancelled');
+--
+-- ALTER TABLE simpipe.runs ADD CONSTRAINT
+--   run_status_fkey FOREIGN KEY (status) REFERENCES simpipe.run_status;
+--
+-- ALTER TABLE simpipe.steps ADD CONSTRAINT
+--   run_status_fkey FOREIGN KEY (status) REFERENCES simpipe.step_status;
