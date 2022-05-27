f1f020e746b31fe182fda27578b550769005b28b
AleenaThomas_gh <aleena.thomas@sintef.no>
2022-05-27 14:52:01 +0200

    Rearranged folder structure
    - moved all files related to backend in the controller folder

--- controller/src/server/server.ts
+++ controller/src/server/server.ts
@@ -6,7 +6,7 @@ import * as functions from './functions.js';
 process.env.IS_SIMULATION_RUNNING = 'false';
 process.env.CANCEL_RUN_LIST = '';
 
-// schema
+// all API endpoints
 const typeDefs = gql`
   type Query {
       All_Simulations: String
@@ -14,8 +14,9 @@ const typeDefs = gql`
       Get_Simulation_Run_Results(simulation_id: String, run_id:String): String
   }
   type Mutation {
+    # TODO: pipeline file as input
     Create_Simulation(model_id:String, name:String): String
-    Create_Run(simulation_id: String, dsl:String, name:String): String
+    # Create_Run(simulation_id: String, dsl:String, name:String): String
     Create_Run_WithInput(simulation_id: String, dsl:String, name:String, sampleInput:[[String]]):
      String
     Start_Run(run_id:String): String
@@ -37,10 +38,10 @@ const resolvers = {
       :Promise<string> {
       return await functions.createSimulation(arguments_.model_id, arguments_.name);
     },
-    async Create_Run(_p: unknown, arguments_: { simulation_id:string, dsl:string, name:string })
-      :Promise<string> {
-      return await functions.createRun(arguments_.simulation_id, arguments_.dsl, arguments_.name);
-    },
+    // async Create_Run(_p: unknown, arguments_: { simulation_id:string, dsl:string, name:string })
+    //   :Promise<string> {
+    //   return await functions.createRun(arguments_.simulation_id, arguments_.dsl, arguments_.name);
+    // },
     async Create_Run_WithInput(_p: unknown, arguments_:
     {
       simulation_id:string,
@@ -69,5 +70,6 @@ await server.listen({ port: 9000, hostname: '0.0.0.0' }).then(({ url }) => {
   // eslint-disable-next-line no-console
   console.log(`🚀  Server ready at ${url}`);
 });
+
 // create a sample simulation at startup
-logger.info(await functions.createSampleSimulation());
+// logger.info(await functions.createSampleSimulation());
