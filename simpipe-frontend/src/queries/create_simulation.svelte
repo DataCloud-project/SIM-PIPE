f5ed04c5cace292ffe92500afd768b19e6fa229f
AleenaThomas_gh <aleena.thomas@sintef.no>
2022-09-19 12:01:20 +0200

    Updated API documentation and deleted model_id

--- simpipe-frontend/src/queries/create_simulation.svelte
+++ simpipe-frontend/src/queries/create_simulation.svelte
@@ -3,8 +3,8 @@
     import { gql } from 'graphql-request'
 
     export const create_simulation_mutation = gql`
-        mutation Create_Simulation($model_id: String, $name: String, $pipeline_description: String) {
-            Create_Simulation(model_id: $model_id, name: $name, pipeline_description: $pipeline_description)
+        mutation Create_Simulation($name: String, $pipeline_description: String) {
+            Create_Simulation(name: $name, pipeline_description: $pipeline_description)
         }   
         `
 
