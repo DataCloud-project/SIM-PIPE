57c4f0d7a149c329c3d9d8b96d3fc81b37471259
Aleena Thomas <84079188+AleenaThomas-gh@users.noreply.github.com>
2023-06-07 13:23:31 +0200

    Modified converter to support defpipe response change
    - adapated converter to defpipe response which has now 'previous' indicating dependencies between steps

--- converter/app/converter_to_argo.py
+++ converter/app/converter_to_argo.py
@@ -16,22 +16,25 @@ def convert(response):
     if( response is None):
         return "error", "Given pipeline description is empty"
     steplist = []
+    ''' Regex: pipeline name'''
     pipeline_name = re.finditer('Pipeline [a-zA-Z]+', response)
     pipeline_name_extracted = ''
     for match in pipeline_name:
         pipeline_name_extracted = match.group().split(' ')[1]
     pipeline_description = init(pipeline_name_extracted)
 
-    # extract relevant information using string matching
+    ''' Regex: step name'''
     matched_stepnames = list(re.finditer('step [a-zA-Z_]+', response))
+    ''' Regex: image names of steps'''
     matched_imagenames = list(re.finditer('implementation: container-implementation\s*image:\s*\'+(.+)\'+', response))
+    ''' Regex: previous/dependency of steps'''
+    matched_dependencies = list(re.finditer('previous:\s*\[(([a-zA-Z\s,_]+))\]', response))
     response = re.sub('\n', ' ', response)
     response = re.sub('\t', ' ', response)
+    ''' Regex: env list of steps'''
     matched_envlist = list(re.finditer("step ([a-zA-Z_]+)[^{]*environmentParameters: ({[^}]+})", response))
-   
-    if( not (len(matched_stepnames) == len(matched_imagenames))):
-        return "error", "Given pipeline description is not valid"
-
+    if( not (len(matched_stepnames) == len(matched_imagenames) == (len(matched_dependencies) + 1))):
+        return "error", "Given pipeline description is not valid/complete"
     # go through each set of matches and assign attributes to current step
     env_list_index = 0 # every step need not have an env_list, so a separate index to retreive matched env listpy
     for index, matched_name in enumerate(matched_stepnames):
@@ -39,20 +42,16 @@ def convert(response):
         stepname = matched_name.group().split(' ')[1]
         # step name
         steplist[index]['name'] = re.sub('[^0-9a-zA-Z-]+', '', stepname)
-
-        """
-        Dependency is not provided in def-pipe response currently.
-        Prerequisite is assumed as the order of appearance.
-        TODO: this should be changed when DEF-PIPE gets updated with relavant values
-        """ 
+      
+        # dependencies
         if index > 0:
-            steplist[index]["dependencies"] = [steplist[index-1]["name"]]
+            steplist[index]["dependencies"] = re.sub('(_|\s)*', '', matched_dependencies[index-1][1]).split(',')
 
         pipeline_description["spec"]["templates"].append({})
 
         # image name
         if(matched_imagenames[index].group().split(' ')[1] == 'container-implementation'):  
-            imagename = re.sub('[^0-9a-zA-Z-/]+', '', matched_imagenames[index].group().split(' ')[3])
+            imagename = re.sub('[^0-9a-zA-Z-/:.]+', '', matched_imagenames[index].group().split(' ')[3])
             valid_argo_name = re.sub('[^0-9a-zA-Z-]+', '', matched_imagenames[index].group().split(' ')[3])
             steplist[index]['template'] = valid_argo_name + '-template' + str(index)
             pipeline_description["spec"]["templates"][index]['name'] = valid_argo_name + '-template' + str(index)
@@ -61,9 +60,7 @@ def convert(response):
             # TODO: this has to be replaced to make it work without 'command'
             pipeline_description["spec"]["templates"][index]['container']['command'] = [ 'sh' ]
 
-        # env 
-        # check if the current step has any env list specified
-        # if(matched_envlist[env_list_index][1] == pipeline_description["spec"]["templates"][index]['name']):
+        # env list (if present for the step)
         if(matched_envlist[env_list_index][1] == stepname):
             matches = re.split(r' |: |{|}|\'|,', matched_envlist[env_list_index][2])
             matches = list(filter(None, matches))
@@ -75,21 +72,54 @@ def convert(response):
             env_list_index += 1 # increase env_list only if matched
     
     pipeline_description["spec"]["templates"].append({'name': 'steps', 'dag': {'tasks': steplist}})
-
-    with open(r'output.yaml', 'w') as file:
-        documents = yaml.dump(pipeline_description, file)
+    # uncomment if needed to yaml file as output
+    # with open(r'output.yaml', 'w') as file:
+    # documents = yaml.dump(pipeline_description, file)
     return pipeline_description
 
-# testing with def-pipe response format
-# response with actual jot container images (can be used once secret is implemented)
-# response = "Pipeline Jot_Pipeline {\n\tcommunicationMedium: medium \n\tsteps:\n\t\t- data-processing step DataCleaning_Conv\n\t\t\timplementation: container-implementation image: 'flaskappconv:latest'\n\t\t\tenvironmentParameters: {\n\t\t\t\t: 'conversions'\n\t\t\t}\n\t\t\tresourceProvider: misha\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\n\n\t\t- data-processing step DataCleaning_Stats\n\t\t\timplementation: container-implementation image: 'flaskappstat:latest'\n\t\t\tenvironmentParameters: {\n\t\t\t\t: 'statistics'\n\t\t\t}\n\t\t\tresourceProvider: misha\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\n\n\t\t- data-processing step DataCleaning_Rev\n\t\t\timplementation: container-implementation image: 'flaskapprev:latest'\n\t\t\tenvironmentParameters: {\n\t\t\t\t: 'revenue'\n\t\t\t}\n\t\t\tresourceProvider: misha\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\n}\n\n"
-
 # response with docker/whalesay images
-defpipe_response = "Pipeline Jot_Pipeline {\n\tcommunicationMedium: medium \n\tsteps:\n\t\t- data-processing step DataCleaning_Conv\n\t\t\timplementation: container-implementation image: 'docker/whalesay'\n\t\t\tenvironmentParameters: {\n\t\t\t\tenv2: 'conversions'\n\t\t\t}\n\t\t\tresourceProvider: misha\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\n\n\t\t- data-processing step DataCleaning_Stats\n\t\t\timplementation: container-implementation image: 'docker/whalesay'\n\t\t\tenvironmentParameters: {\n\t\t\t\tenv3: 'statistics'\n\t\t\t}\n\t\t\tresourceProvider: misha\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\n\n\t\t- data-processing step DataCleaning_Rev\n\t\t\timplementation: container-implementation image: 'docker/whalesay'\n\t\t\tenvironmentParameters: {\n\t\t\t\tenv1: 'revenue'\n\t\t\t}\n\t\t\tresourceProvider: misha\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\n}\n\n"
+# defpipe_response = "Pipeline Pilot Pipeline for Testing  {\n\tcommunicationMedium: medium \n\tsteps:\n\t\t- data-processing step Data_Analysis\n\t\t\timplementation: container-implementation image: 'data-analysis'\n\t\t\tenvironmentParameters: {\n\n\t\t\t}\n\t\t\tresourceProvider: \n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\t\t\t\t\tnetworkRequirement:\n\t\t\t\t\t\tmin-bandwidth: 20\n\t\t\t\t\t\tmax-bandwidth: 100\n\t\t\t\t\t\tmin-latency: 10\n\t\t\t\t\t\tmax-latency: 20\n\n\n\t\t- data-processing step Digital_Twins\n\t\t\timplementation: container-implementation image: 'simulation'\n\t\t\tenvironmentParameters: {\n\n\t\t\t}\n\t\t\tresourceProvider: \n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\t\t\t\t\thorizontalScale:\n\t\t\t\t\t\tmin-instance: 10\n\t\t\t\t\t\tmax-instance: 1000\n\t\t\tprevious: [Data Analysis]\n\n\t\t- data-processing step What-If_Analysis\n\t\t\timplementation: container-implementation image: 'what-if-analysis'\n\t\t\tenvironmentParameters: {\n\n\t\t\t}\n\t\t\tresourceProvider: \n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\t\t\t\t\tnetworkRequirement:\n\t\t\t\t\t\tmin-bandwidth: 30\n\t\t\t\t\t\tmax-bandwidth: 100\n\t\t\t\t\t\tmin-latency: 20\n\t\t\t\t\t\tmax-latency: 30\n\t\t\tprevious: [Digital Twins]\n}\n\nCloudProvider Cloud Service {\n\tproviderLocation: 'x'\n\tmappingLocation: 'x'\n}\n\nEdgeProvider 2nd {\n\tproviderLocation: 'aa'\n\tmappingLocation: 'aa'\n}"
+defpipe_response = "Pipeline Jot_Pipeline {\n\tcommunicationMedium: medium \n\tsteps:\n\t\t- data-processing step DataCleaning_Conv\n\t\t\timplementation: container-implementation image: 'flaskappconv:latest'\n\t\t\tenvironmentParameters: {\n\t\t\t\t: 'conversions'\n\t\t\t}\n\t\t\tresourceProvider: misha\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\n\n\t\t- data-processing step DataCleaning_Stats\n\t\t\timplementation: container-implementation image: 'flaskappstat:latest'\n\t\t\tenvironmentParameters: {\n\t\t\t\t: 'statistics'\n\t\t\t}\n\t\t\tresourceProvider: misha\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\t\t\tprevious: [DataCleaning_Conv]\n\n\t\t- data-processing step DataCleaning_Rev\n\t\t\timplementation: container-implementation image: 'flaskapprev:latest'\n\t\t\tenvironmentParameters: {\n\t\t\t\t: 'revenue'\n\t\t\t}\n\t\t\tresourceProvider: misha\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\t\t\t\t\t\n\t\t\tprevious: [DataCleaning_Conv, DataCleaning_Stats]\n}\n\n"
 argo_data = convert(defpipe_response)
 
 yaml_output = yaml.dump(argo_data, sort_keys=False) 
 
 print(yaml_output) 
-
-# problem with dependencies, check list notation
+''' example output
+apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+metadata:
+  generateName: test-pipeline-
+spec:
+  entrypoint: steps
+  templates:
+  - name: flaskappconvlatest-template0
+    container:
+      image: flaskappconv:latest
+      command:
+      - sh
+  - name: flaskappstatlatest-template1
+    container:
+      image: flaskappstat:latest
+      command:
+      - sh
+  - name: flaskapprevlatest-template2
+    container:
+      image: flaskapprev:latest
+      command:
+      - sh
+  - name: steps
+    dag:
+      tasks:
+      - name: DataCleaningConv
+        template: flaskappconvlatest-template0
+      - name: DataCleaningStats
+        dependencies:
+        - DataCleaningConv
+        template: flaskappstatlatest-template1
+      - name: DataCleaningRev
+        dependencies:
+        - DataCleaningConv
+        - DataCleaningStats
+        template: flaskapprevlatest-template2
+'''
