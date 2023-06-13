9bba110564b177868e5725f7d04fdedeb21072ba
Gøran Brekke Svaland <goran.svaland@sintef.no>
2023-06-13 13:27:09 +0200

    add lib folder

---
+++ frontend-dev/src/lib/KubernetesSecret.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    let secret: { [key: string]: string} = {
+        name: 'kube-secret',
+        username: 'kube-username',
+        password: 'kube-password',
+        host: 'kube-host',
+    };
+
+    let checked: boolean = false;
+
+    function returnDots(input_text: string) {
+        let star = "*";
+        return star.repeat(input_text.length)
+    }
+    </script>
+
+<!-- Visual part of component -->
+<div class="table-container">
+    <header class="card-header container-fluid">
+        <div align="left">{ secret.name }</div>
+        <div align="right"><input type=checkbox checked={checked}></div>
+    <table class="table table-hover">
+        {#each Object.keys(secret) as key}
+            <tr>
+                {#if key === 'password'}
+                    <td>{key}: {returnDots(secret[key])}</td>
+                {:else}
+                    <td>{key}: {secret[key]}</td>
+                {/if}
+            </tr>
+        {/each}
+    </table>
+</div>
