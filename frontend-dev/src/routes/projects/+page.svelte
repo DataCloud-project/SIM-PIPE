a2d596c8a289f3bca3b60d591b9da3c4f1dce7f5
AleenaThomas_gh <aleena.thomas@sintef.no>
2023-06-16 11:26:18 +0200

    Added checkboxes in projects page

--- frontend-dev/src/routes/projects/+page.svelte
+++ frontend-dev/src/routes/projects/+page.svelte
@@ -1,13 +1,21 @@
 <script lang='ts'>
     import { graphQlClient } from "../../stores/stores";
+	import { onMount } from 'svelte';
     import projects from '../../stores/projects.json';
     //let projects: any[] = [];
     function getProjectsList() {
         //projects = graphQlClient.request(all_projects_query)
-        // TODO replace with actual query
+        // TODO: replace with actual query
         console.log("hello world")
     }
-    let project_id = '10';
+    let checkboxes: Record<string, boolean> = {};
+
+	// onMount add dry_run.names to checkboxes
+	onMount(() => {
+		projects.forEach((element) => {
+			checkboxes[element.name] = false;
+		});
+	});
 </script>
 
 
@@ -19,9 +27,10 @@
 <div class="p-5 table-container">
 	<!-- Native Table Element -->
     <!-- TODO add margin/padding for table elements -->
-	<table class="table table-hover">
+	<table class="table table-interactive">
 		<thead>
 			<tr>
+				<th />
 				<th>Name</th>
 				<th>Created date</th>
 				<th>Dry runs</th>
@@ -30,11 +39,12 @@
 		</thead>
 		<tbody>
 			{#each projects as project}
-                <tr class="clickable"  onclick="window.location=`/projects/[project_id]/{project.project_id}`">
-                        <td>{project.name}</td>
-                        <td>{project.created_date}</td>
-                        <td>{project.dry_run_count}</td>
-                        <td>{project.simulations_count}</td>
+                <tr class="clickable table-row-checked"  onclick="window.location=`/projects/[project_id]/{project.project_id}`">
+                    <td><input type="checkbox" bind:checked={checkboxes[project.name]} class="checkbox" /></td>
+                    <td>{project.name}</td>
+                    <td>{project.created_date}</td>
+                    <td>{project.dry_run_count}</td>
+                    <td>{project.simulations_count}</td>
                 </tr>
 			{/each}
 		</tbody>
