eedae57bfdc80f3c81eb4bdc9bc781e7e7b41f39
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2023-06-21 14:36:20 +0200

    ⚡️ Huge performance improvement. Using VictoriaMetrics' parser instead of Prometheus' parser.
    Thanks to @fmauNeko for the hint.

--- cadvisor-bridge/main.go
+++ cadvisor-bridge/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
@@ -12,6 +13,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/common/expfmt"
+
+	vmp "github.com/VictoriaMetrics/VictoriaMetrics/lib/protoparser/prometheus"
 )
 
 // constants
@@ -32,20 +35,31 @@ var SIMPIPE_LABELS = []string{
 	"full_name",      // name
 }
 
-func fetchMetrics(endpoint string) (map[string]*dto.MetricFamily, error) {
+func fetchMetrics(endpoint string) (string, error) {
 	resp, err := http.Get(endpoint)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	defer resp.Body.Close()
 
-	var parser expfmt.TextParser
-	parsed, err := parser.TextToMetricFamilies(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	return parsed, nil
+	stringMetrics := string(body)
+	return stringMetrics, nil
+}
+
+func parseMetricsSlowly(data string) (map[string]*dto.MetricFamily, error) {
+	var parser expfmt.TextParser
+	return parser.TextToMetricFamilies(strings.NewReader(data))
+}
+
+func parseMetricsQuickly(data string) *vmp.Rows {
+	var rows vmp.Rows
+	rows.Unmarshal(data)
+	return &rows
 }
 
 func main() {
@@ -57,23 +71,64 @@ func main() {
 	}
 	endpoint := os.Args[1]
 
-	// TODO: apply your filters here
+	// create a struct that holds the help and the metric type (dto.MetricType)
+	type metricMetadata struct {
+		help string
+		typ  dto.MetricType
+	}
+
+	// Create a map of metric names to their metadata
+	metricMetadataMap := make(map[string]metricMetadata)
+	mapIsInitialized := false
 
 	// Expose the metrics
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		// prometheus.DefaultRegisterer.UnregisterAll() // Clear all the old metrics
 
-		metrics, err := fetchMetrics(endpoint)
+		metricsString, err := fetchMetrics(endpoint)
 		if err != nil {
 			http.Error(w, "Error fetching metrics", http.StatusInternalServerError)
 			return
 		}
 
+		if !mapIsInitialized {
+			// parse the metrics using the slow prometheus parser, that parses more metadata information
+			// we do it only once because it is slow
+			metricsWithMetadata, err := parseMetricsSlowly(metricsString)
+			if err != nil {
+				http.Error(w, "Error parsing metrics", http.StatusInternalServerError)
+				return
+			}
+			for _, metric := range metricsWithMetadata {
+				metricName := metric.GetName()
+				if strings.HasPrefix(metricName, "container_") {
+					// get the help and the type of the metric
+					help := metric.GetHelp()
+					metricType := metric.GetType()
+
+					// store the metadata in the map
+					metricMetadataMap[metricName] = metricMetadata{help, metricType}
+				}
+			}
+			mapIsInitialized = true
+		}
+
+		metrics := parseMetricsQuickly(metricsString)
+
 		collectors := make([]prometheus.Collector, 0)
 
-		for _, metric := range metrics {
+		metricsMap := make(map[string]([]vmp.Row))
 
-			metricName := metric.GetName()
+		for _, row := range metrics.Rows {
+			metricName := row.Metric
+			metric, found := metricsMap[metricName]
+			if !found {
+				metric = make([]vmp.Row, 0)
+			}
+			metricsMap[metricName] = append(metric, row)
+		}
+
+		for metricName, rows := range metricsMap {
 
 			// starts with container_
 			if strings.HasPrefix(metricName, "container_") {
@@ -82,11 +137,11 @@ func main() {
 				var simpipeMetrics map[string]*prometheus.Labels = make(map[string]*prometheus.Labels)
 
 				hasAtLeastOneSimpipe := false
-				for _, serie := range metric.Metric {
+				for _, serie := range rows {
 					// check if it's a simpipe followed metric by checking the label container_label_simpipe
 					isSimpipe := false
-					for _, label := range serie.Label {
-						if label.GetName() == SIMPIPE_LABEL && label.GetValue() == "true" {
+					for _, label := range serie.Tags {
+						if label.Key == SIMPIPE_LABEL && label.Value == "true" {
 							isSimpipe = true
 							hasAtLeastOneSimpipe = true
 							break
@@ -97,9 +152,9 @@ func main() {
 					if isSimpipe {
 						key := ""
 						labels := make(prometheus.Labels, 5)
-						for _, label := range serie.Label {
-							labelName := label.GetName()
-							labelValue := label.GetValue()
+						for _, label := range serie.Tags {
+							labelName := label.Key
+							labelValue := label.Value
 							switch labelName {
 							case "container_label_annotation_workflows_argoproj_io_node_id":
 								labels["node_id"] = labelValue
@@ -125,14 +180,22 @@ func main() {
 					continue
 				}
 
-				help := metric.GetHelp()
+				// fetch the metadata from this metric
+				metadata, found := metricMetadataMap[metricName]
+				var help string
+				var metricType dto.MetricType
+				if found {
+					help = metadata.help
+					metricType = metadata.typ
+				} else {
+					help = "Undocumented metric, consider restarting the cadvisor-bridge"
+					metricType = dto.MetricType_GAUGE // Most likely a gauge
+				}
 
 				// Create a gauge for the metric
 				simpipeName := "simpipe_" + strings.TrimPrefix(metricName, "container_")
 
 				// create the right collector
-
-				metricType := *metric.Type
 				var gauge *prometheus.GaugeVec
 				var counter *prometheus.CounterVec
 				switch metricType {
@@ -149,18 +212,18 @@ func main() {
 					}, SIMPIPE_LABELS)
 					collectors = append(collectors, counter)
 				default:
-					fmt.Println("Unknown metric type: ", metric.Type)
+					fmt.Println("Unknown metric type: ", metricType.String())
 					continue
 				}
 
 				// Yes this is a label
 			metric_loop:
-				for _, serie := range metric.Metric {
+				for _, serie := range rows {
 					labels := prometheus.Labels{}
 
-					for _, label := range serie.Label {
-						labelName := label.GetName()
-						labelValue := label.GetValue()
+					for _, label := range serie.Tags {
+						labelName := label.Key
+						labelValue := label.Value
 						switch labelName {
 						case "container_label_io_kubernetes_pod_uid":
 							// fill the labels with the ones from the simpipeMetrics
@@ -184,12 +247,12 @@ func main() {
 
 					switch metricType {
 					case dto.MetricType_GAUGE:
-						gauge.With(labels).Set(serie.GetGauge().GetValue())
+						gauge.With(labels).Set(serie.Value)
 					case dto.MetricType_COUNTER:
-						counter.With(labels).Add(serie.GetCounter().GetValue())
+						counter.With(labels).Add(serie.Value)
 					default:
 						// crash as we should not be here
-						panic("Unknown metric type: " + metric.Type.String())
+						panic("Unknown metric type: " + metricType.String())
 					}
 				}
 			}
