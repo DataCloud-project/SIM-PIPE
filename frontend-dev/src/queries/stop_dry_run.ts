765e153ea1b9dd2ec12729d23248ac72b66c9c8a
AleenaThomas_gh <aleena.thomas@sintef.no>
2023-07-10 17:28:57 +0200

    WIP: Added stop dry run

--- frontend-dev/src/queries/stop_dry_run.ts
+++ frontend-dev/src/queries/stop_dry_run.ts
@@ -1,8 +1,13 @@
 import { gql } from 'graphql-request';
 
 const stopDryRunMutation = gql`
-  mutation stopDryRun($dryRunId: String, terminate:boolean) {
-    stopDryRun(dryRunId: $dryRunId)
+  mutation stopDryRun($dryRunId: String!, $terminate:Boolean) {
+    stopDryRun(dryRunId: $dryRunId, terminate: $terminate) {
+      id
+      status {
+        phase
+      }
+    }
   }
 `;
 
