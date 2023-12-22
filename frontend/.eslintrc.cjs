c00f38980da1b853439f64ce17c84970d99fde67
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2023-12-22 13:35:40 +0100

    🔧 Setting up eslint correctly

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,17 +1,27 @@
+/** @type { import("eslint").Linter.FlatConfig } */
 module.exports = {
 	root: true,
 	extends: [
-		'eslint:recommended',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
+		'plugin:eslint-comments/recommended',
 		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'airbnb-base',
+		'airbnb-typescript/base',
+		'plugin:unicorn/recommended',
 		'plugin:svelte/recommended',
 		'prettier'
 	],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	plugins: ['@typescript-eslint', 'simple-import-sort'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+		extraFileExtensions: ['.svelte'],
+		tsconfigRootDir: __dirname,
+		project: './tsconfig.json',
+		warnOnUnsupportedTypeScriptVersion: false
 	},
 	env: {
 		browser: true,
@@ -28,8 +38,31 @@ module.exports = {
 		}
 	],
 	rules: {
-		// TODO remove those rules
-		'@typescript-eslint/no-unused-vars': 'warn',
-		'@typescript-eslint/no-explicit-any': 'warn'
+		'import/extensions': 'off',
+		'import/no-unresolved': 'off',
+		'import/first': 'warn',
+		'import/newline-after-import': 'warn',
+		'import/no-duplicates': 'warn',
+		'import/no-extraneous-dependencies': 'off',
+		'simple-import-sort/imports': ['warn', {
+			groups: [
+				['^\\u0000'],
+				['^@?\\w', '^@?\\w.*\\u0000$'],
+				['(?<!\\u0000)$', '(?<=\\u0000)$'],
+				['^\\.', '^\\..*\\u0000$'],
+			],
+		}],
+		'import/prefer-default-export': 'off',
+		'import/no-mutable-exports': 'off',
+		'simple-import-sort/exports': 'warn',
+		'@typescript-eslint/explicit-function-return-type': 'error',
+		'no-return-await': 'off',
+		'@typescript-eslint/return-await': 'off',
+		'indent': 'off',
+		'function-paren-newline': 'off',
+		'@typescript-eslint/consistent-type-imports': ['warn', {
+			prefer: 'type-imports',
+		}],
+		'unicorn/no-useless-undefined': 'off',
 	}
 };
