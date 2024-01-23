0e3d5fbfa0d6229a3ff3d7f0e3c27083c5122290
Aleena Thomas <84079188+AleenaThomas-gh@users.noreply.github.com>
2024-01-23 16:20:35 +0100

    Added support for argo template field env
    - added env in create new dry run modal
    - added option to change env arguments from argo yaml file

--- frontend/src/routes/projects/dryruns/[dry_run]/modal-submit-new-dry-run.svelte
+++ frontend/src/routes/projects/dryruns/[dry_run]/modal-submit-new-dry-run.svelte
@@ -4,7 +4,7 @@
 	import { selectedProject } from '../../../../stores/stores.js';
 	import createDryRunMutation from '../../../../queries/create_dry_run.js';
 	import allDryRunsQuery from '../../../../queries/get_all_dryruns.js';
-	import type { Project, Template, Task } from '../../../../types.js';
+	import type { Project, Template, Task, Templates } from '../../../../types.js';
 	import refreshProjectDetails from '../../../../lib/refresh_runs.js';
 	import { requestGraphQLClient } from '$lib/graphqlUtils.js';
 
@@ -24,11 +24,14 @@
 	};
 
 	// stores the names of input files expected for initial steps while parsing
-	const input_artifacts: any = {};
-	let initial_task_name = ''; //TODO: make this a list to accomodate multiple initial steps in the pipeline
-	let initial_task_index = -1; //TODO: make this a list to accomodate multiple initial steps in the pipeline
-	const taskList = parseTaskList() || [];
+	const { tasks, templates, inputArtifacts, initialTaskName } = parseTaskList();
 	let alertModal = false;
+
+	function findTemplateByTaskName(taskTemplate: string) {
+		if (templates) {
+			return templates.find((template) => template.name === taskTemplate);
+		}
+	}
 
 	function parseTaskList() {
 		// only valid for dag format https://argoproj.github.io/argo-workflows/walk-through/dag/
@@ -46,43 +49,44 @@
 			formData.dagTemplate = templates.find((template: Template) => template.dag);
 			const tasks: Task[] = formData.dagTemplate ? formData.dagTemplate.dag?.tasks : [];
 			// TODO: extend; find the initial step (currently assuming there is only 1 initial step)
-			for (let task of tasks) {
-				if (!task.dependencies) {
-					// initial step has no dependencies
-					initial_task_name = task.name;
-					// initial_task_index = index;
-					break;
-				}
-			}
+			const initialTask = tasks.find((task) => !task.dependencies);
 
 			// extract input artifacts from initial step in the template
-			templates.forEach((template: { name: string; inputs: any }) => {
-				// TODO: only 1 initial input file is considered
-				if (template.name == initial_task_name) {
-					if (template.inputs.artifacts?.[0].s3) {
-						// the input file is of type s3
-						input_artifacts[template.name] = { inputs: template.inputs, type: FileUploadType.S3 };
-					} else if (template.inputs.artifacts?.[0].raw) {
-						// the input file is of type raw
-						input_artifacts[template.name] = { inputs: template.inputs, type: FileUploadType.Raw };
+			const inputArtifacts: any = {};
+			if (initialTask) {
+				templates.forEach((template: { name: string; inputs: { artifacts: any[] } }) => {
+					if (template.name === initialTask.name && template.inputs.artifacts?.[0]) {
+						const artifact = template.inputs.artifacts[0];
+						inputArtifacts[template.name] = {
+							inputs: template.inputs,
+							type: artifact.s3 ? FileUploadType.S3 : artifact.raw ? FileUploadType.Raw : undefined
+						};
 					}
-				}
-			});
-			return tasks;
+				});
+			}
+
+			const templates1: Templates[] = templates;
+			return {
+				tasks,
+				templates: templates1,
+				inputArtifacts,
+				initialTaskName: initialTask?.name || ''
+			};
 		}
+		return { tasks: [], templates: undefined, inputArtifacts: [], initialTaskName: '' };
 	}
 
 	// modify workflow template from project to create a valid argoWorkflow input for create new dryrun
 	async function newWorkflowTemplate(template: { metadata: any; spec: any }) {
 		const newWorkflowTemplate = template;
-		if (Object.keys(input_artifacts).length != 0) {
+		if (Object.keys(inputArtifacts).length != 0) {
 			// if pipeline has initial input files
 			try {
-				for (const [index, artifact] of input_artifacts[
-					initial_task_name
+				for (const [index, artifact] of inputArtifacts[
+					initialTaskName
 				]?.inputs.artifacts.entries()) {
 					// for input file type = raw, replace the raw contents in the workflow template
-					if (input_artifacts[initial_task_name].type == FileUploadType.Raw) {
+					if (inputArtifacts[initialTaskName].type == FileUploadType.Raw) {
 						const files = formData.files[index] as unknown as FileList;
 						if (files) {
 							let text = await files[0].text();
@@ -90,7 +94,7 @@
 						} else {
 							throw new Error('Initial input file is not uploaded!');
 						}
-					} else if (input_artifacts[initial_task_name].type == FileUploadType.S3) {
+					} else if (inputArtifacts[initialTaskName].type == FileUploadType.S3) {
 						artifact.s3.key = formData.s3Key;
 					}
 				}
@@ -107,7 +111,7 @@
 		// TODO: current fix is to have the user enter the input filesize manually
 		newWorkflowTemplate.metadata.annotations = {
 			filesize: formData.filesize,
-			initial_task_name: initial_task_name
+			initial_task_name: initialTaskName
 		};
 		return newWorkflowTemplate;
 	}
@@ -125,7 +129,6 @@
 					argoWorkflow: modifiedWorkflowTemplate
 				}
 			};
-
 			const responseCreateDryRun: { createDryRun: { id: string } } = await requestGraphQLClient(
 				createDryRunMutation,
 				variables
@@ -187,16 +190,10 @@
 				/>
 			</label>
 
-			{#if taskList.length > 0}
-				{#each taskList as task, i}
+			{#if tasks.length > 0}
+				{#each tasks as task, i}
 					<div class="ml-5">
-						<!-- svelte-ignore a11y-label-has-associated-control -->
-						<label><b>{i + 1}. {task.name}</b></label>
-						<!-- <label
-							>Template: <span>
-								<input class="variant-soft-surface" type="text" value={task.template} readonly />
-							</span></label
-						> -->
+						<p><b>{i + 1}. {task.name}</b></p>
 						{#if task.dependencies}
 							<label
 								>Dependencies: <span>
@@ -210,12 +207,12 @@
 							>
 						{/if}
 						<!-- input artifact option for initial step in the pipeline -->
-						{#if task.name == initial_task_name}
+						{#if task.name == initialTaskName && inputArtifacts?.[task.name]}
 							<br />
 							<!-- svelte-ignore a11y-label-has-associated-control -->
-							{#if input_artifacts?.[task.name]?.type == FileUploadType.Raw}
+							{#if inputArtifacts?.[task.name]?.type == FileUploadType.Raw}
 								<p>Upload Input files</p>
-								{#each input_artifacts[task.name].inputs.artifacts || [] as artifact, k}
+								{#each inputArtifacts[task.name].inputs.artifacts || [] as artifact, k}
 									<!-- svelte-ignore a11y-label-has-associated-control -->
 									<label>
 										<span>
@@ -225,8 +222,8 @@
 									</label>
 									<br />
 								{/each}
-							{:else if input_artifacts?.[task.name]?.type == FileUploadType.S3}
-								{#each input_artifacts[task.name].inputs.artifacts || [] as artifact}
+							{:else if inputArtifacts?.[task.name]?.type == FileUploadType.S3}
+								{#each inputArtifacts[task.name].inputs.artifacts || [] as artifact}
 									<!-- svelte-ignore a11y-label-has-associated-control -->
 									<label>
 										Enter S3 key for file input: <span class="italic">{artifact.name} </span>
@@ -246,20 +243,32 @@
 								</span>
 							</label>
 						{/if}
-						{#each taskList[i].arguments?.parameters || [] as param, j}
-							<!-- svelte-ignore a11y-label-has-associated-control -->
-							<label>Enviroment parameters:</label>
+						<!-- if dry run has arguments/parameters argo fields -->
+						{#each tasks[i].arguments?.parameters || [] as param}
+							<p>Enviroment parameters:</p>
 							<label class="ml-5"
 								>{param.name}:
 								<span>
-									<input
-										class="input variant-form-material"
-										type="text"
-										bind:value={taskList[i].arguments.parameters[j].value}
-									/>
+									<input class="input variant-form-material" type="text" bind:value={param.value} />
 								</span></label
 							>
 						{/each}
+						<!-- if dry run has env/name/value argo fields -->
+						{#if templates}
+							{#each findTemplateByTaskName(tasks[i].template)?.container?.env || [] as param}
+								<p>Enviroment parameters:</p>
+								<label class="ml-5"
+									>{param.name}:
+									<span>
+										<input
+											class="input variant-form-material"
+											type="text"
+											bind:value={param.value}
+										/>
+									</span></label
+								>
+							{/each}
+						{/if}
 					</div>
 				{/each}
 			{/if}
