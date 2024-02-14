a8a7df75693b98e6a2279c65a669a16106938a3c
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-02-14 15:09:38 +0100

    update: add project info

--- frontend/src/queries/get_workflow_template.ts
+++ frontend/src/queries/get_workflow_template.ts
@@ -3,6 +3,8 @@ import { gql } from 'graphql-request';
 const getWorkflowQuery = gql`
 	query WorkflowTemplate($name: String!) {
 		workflowTemplate(name: $name) {
+			name
+			project {name, id}
 			argoWorkflowTemplate
 		}
 	}
