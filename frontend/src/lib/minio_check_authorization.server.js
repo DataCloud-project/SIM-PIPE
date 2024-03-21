01cad0cd725c4688e0dac5f65b7396153821009d
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-03-21 16:11:27 +0100

    fix: remove unused api - features moved to controller

--- frontend/src/lib/minio_check_authorization.server.js
+++
@@ -1,6 +0,0 @@
-import { SECRET_API_KEY } from "$env/static/private"
-
-export function check_authorization(request) {
-    const authHeaders = request.headers.get('Authorization')
-    return authHeaders && authHeaders === 'Bearer ' + SECRET_API_KEY
-}
