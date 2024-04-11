d1ab56ffe7aaf703deea86d6ef82ec2521082d6d
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-04-11 11:12:42 +0200

    fix: build stage - svelte adapter for node servers

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,7 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -16,7 +17,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({ out: 'build' }),
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: true,
+			envPrefix: ''
+		}),
 		alias: {
 			$components: './src/components',
 			$stores: './src/stores',
