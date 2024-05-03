7098763e53818fe2cd4e0a6f893ea2ece8502450
Gøran Brekke Svaland <goran.svaland@sintef.no>
2024-05-03 14:21:55 +0200

    update: initial commit for fr #155

---
+++ controller/src/curve_fitting/curve-fitting.ts
@@ -0,0 +1,113 @@
+// Regression
+// Power Law Curve Fitting (y = a * x ^ b)
+// Linear Curve Fitting (y = a + b * x)
+
+interface CurveFitReturnType { coeffs: number[], type: string, r2: number }
+
+function round(number: number, precision: number): number {
+  // Round a number to a certain number of decimal places
+  const factor = 10 ** precision;
+  return Math.round(number * factor) / factor;
+}
+
+function powerFunction(x: number, a: number, b: number, precision: number): number {
+  // Power law function a * x ^ b
+  return round(a * (x ** b), precision);
+}
+
+function determinationCoefficient(data_y: number[], data_y_predicted: number[]): number {
+  // R^2 value of the power law curve fitting // coefficient of determination
+  const predictions = data_y_predicted.map((prediction) => prediction);
+  const observations = data_y.map((y) => y);
+
+  let sum = 0; // sum of observations
+  for (const observation of observations) {
+    sum += observation;
+  }
+  const mean = sum / observations.length;
+
+  let ssyy = 0; // sum of squares total
+  for (const observation of observations) {
+    const difference = observation - mean;
+    ssyy += difference * difference;
+  }
+
+  let sse = 0; // sum of squared errors
+  for (const [index, observation] of observations.entries()) {
+    const prediction = predictions[index];
+    const residual = observation - prediction;
+    sse += residual * residual;
+  }
+
+  return 1 - (sse / ssyy);
+}
+
+function power(data_x: number[], data_y: number[], precision: number): CurveFitReturnType {
+  // Perform the power regression of power law data a * x ^ b
+  const sum = [0, 0, 0, 0, 0];
+  const nvalues = data_x.length;
+  for (let n = 0; n < nvalues; n += 1) {
+    if (data_y[n] !== null) {
+      sum[0] += Math.log(data_x[n]);
+      sum[1] += Math.log(data_y[n]) * Math.log(data_x[n]);
+      sum[2] += Math.log(data_y[n]);
+      sum[3] += (Math.log(data_x[n]) ** 2);
+    }
+  }
+  const b = ((nvalues * sum[1]) - (sum[0] * sum[2])) / ((nvalues * sum[3]) - (sum[0] ** 2));
+  const a = ((sum[2] - (b * sum[0])) / nvalues);
+  const coeffA = round(Math.exp(a), precision);
+  const coeffB = round(b, precision);
+
+  const predictedY = data_x.map((x) => powerFunction(x, coeffA, coeffB, precision));
+
+  return {
+    coeffs: [coeffA, coeffB],
+    type: 'power',
+    r2: round(determinationCoefficient(data_y, predictedY), precision),
+  };
+}
+
+function linear(
+  data_x: number[],
+  data_y: number[]): CurveFitReturnType {
+  // linear regression
+  const x1 = data_x[0];
+  const x2 = data_x[1];
+  const y1 = data_y[0];
+  const y2 = data_y[1];
+  const b = (y2 - y1) / (x2 - x1);
+  const a = y1 - b * x1;
+  return {
+    coeffs: [a, b],
+    type: 'linear',
+    r2: 1,
+  };
+}
+
+// Curve fitting of power law data a * x ^ b
+export default function curveFitting(
+  data_x: number[],
+  data_y: number[],
+  precision = 4): CurveFitReturnType {
+  let results = {
+    coeffs: [0, 0], type: '', r2: 0,
+  };
+
+  if (data_x.length !== data_y.length) {
+    throw new Error('Input data arrays must have the same length');
+  }
+  if (data_x.length < 2) {
+    throw new Error('Input data arrays must have at least 2 data points');
+  }
+  if (data_x.length === 2) {
+    // linear regression
+    results = linear(data_x, data_y);
+  }
+  if (data_x.length > 2) {
+    // power law regression
+    // map data_x and data_y to data array of arrays
+    results = power(data_x, data_y, precision);
+  }
+  return results;
+}
